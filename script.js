faa558b8a9f85a0cb3896c69d5f09f11b8665b7e
karen queiroz <kqueiroz812@gmail.com>
2019-08-02 17:05:01 -0300

    adicionar promisses

---
+++ script.js
@@ -0,0 +1,26 @@
+// let diadaSemana = "sexta";
+
+// callback = (resolver ,reject) =>{
+//      if(diadaSemana == "segunda"){
+//         resolver("Acertou");
+//      }
+//      else{
+//           reject("Erro");
+//       }
+//  }
+
+
+//  let promessa = new Promise(callback);
+
+//  promessa
+//     .then(console.log)
+//     .catch(console.error)
+//     .finally(console.log("funcao concluida"));
+
+
+
+fetch('')
+
+
+
+
